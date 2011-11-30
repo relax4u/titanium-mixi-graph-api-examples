0e43f4245b1dff359f5b391e2541579b33692eb0
Yoshikazu Ozawa <yoshikazu.ozawa@gmail.com>
2011-12-01 00:31:02 +0900

    add Check API

---
+++ Resources/your_app_name/ui/check.js
@@ -0,0 +1,31 @@
+(function(){
+	your_namespace.ui.check = {};
+	
+	your_namespace.ui.check.createWindow = function() {
+		var win = Ti.UI.createWindow($.mixin({
+			title: L("check_api"),
+			layout: 'vertical'
+		}, $$.window));
+		
+		var check = Ti.UI.createButton($.mixin({
+			title: L("check")
+		}, $$.button));
+		check.addEventListener('click', function(){
+			mixi.graphApi.share({
+				parameters: {
+					key: mixi.key,
+					title: "いいね！タイトル",
+					primary_url: mixi.primary_url,
+					privacy: {visibility: "self"}
+				},
+				success: function(json){
+					alert(json);
+				}
+			});
+		});
+		
+		win.add(check);
+		
+		return win;
+	};
+})();
